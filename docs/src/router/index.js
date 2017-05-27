3a081f1e3bac8559a4cfffc771d06a8a5c5ec2fb
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-05-27 14:32:24 +0500

    test plugin install

--- docs/src/router/index.js
+++ docs/src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }]
 })
