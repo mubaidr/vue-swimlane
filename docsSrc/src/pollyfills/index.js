b677983a3e293deb7d4e28f5d99ae1e9ce384b6f
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-05-29 22:24:47 +0500

    Move docs folder, added theme and pollyfills

---
+++ docsSrc/src/pollyfills/index.js
@@ -0,0 +1,7 @@
+import raf from 'raf'
+import smoothscrollPolyfill from 'smoothscroll-polyfill'
+import es6Promise from 'es6-promise'
+
+raf.polyfill()
+smoothscrollPolyfill.polyfill()
+es6Promise.polyfill()
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import debounce from 'lodash.debounce'
 import vueSliderComponent from './vue-slider'
+
+var mixins = {
+  methods: {
+    _debounce: debounce
+  }
+}
 
 const vueSlider = {
   install (Vue, options) {
+    Vue.mixins(mixins)
     Vue.component(vueSliderComponent.name, vueSliderComponent)
   }
 }
