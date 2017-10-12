ba4c23cf806988ba522933d878edb508374ccda9
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-10-12 07:33:19 -0700

    update theme, disable plugin version check before scripts

--- docsSrc/build/dev-server.js
+++ docsSrc/build/dev-server.js
@@ -1,4 +1,4 @@
-require('./check-versions')()
+//require('./check-versions')()
 
 var config = require('../config')
 if (!process.env.NODE_ENV) {
@@ -10,9 +10,9 @@ var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
 var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = process.env.NODE_ENV === 'testing'
-  ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf')
+var webpackConfig = process.env.NODE_ENV === 'testing' ?
+  require('./webpack.prod.conf') :
+  require('./webpack.dev.conf')
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
@@ -36,7 +36,9 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
+    hotMiddleware.publish({
+      action: 'reload'
+    })
     cb()
   })
 })
@@ -45,7 +47,9 @@ compiler.plugin('compilation', function (compilation) {
 Object.keys(proxyTable).forEach(function (context) {
   var options = proxyTable[context]
   if (typeof options === 'string') {
-    options = { target: options }
+    options = {
+      target: options
+    }
   }
   app.use(proxyMiddleware(options.filter || context, options))
 })
